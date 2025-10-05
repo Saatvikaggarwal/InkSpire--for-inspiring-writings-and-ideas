bb3a97dfbc2c6e7c142a21cadaaba09102187077
Saatvikaggarwal <124760998+Saatvikaggarwal@users.noreply.github.com>
2025-10-05 11:01:17 +0530

    Update Adminportal.jsx

--- Client/src/pages/Adminportal.jsx
+++ Client/src/pages/Adminportal.jsx
@@ -32,7 +32,7 @@ const Adminportal = () => {
 
   async function countLikes(postId) {
     try {
-      const res = await axios.get(`http://localhost:4444/api/like/${postId}`, {
+      const res = await axios.get(`https://inkspire-for-inspiring-writings-and.onrender.com/api/like/${postId}`, {
         withCredentials: true,
       });
       setLikeCounts((prev) => ({
