fbfcf653e98c8fce7e584bb20f1281c3254a8c09
Saatvikaggarwal <124760998+Saatvikaggarwal@users.noreply.github.com>
2025-10-05 11:19:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://inkspire-for-inspiring-writings-and-3gyl.onrender.com",
   credentials:true,
 }))
 
