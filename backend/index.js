fe831a4111a0d6e5cfdef932744f3e5fac82bd87
Saatvik <saatik1303@gmail.com>
2025-10-05 20:28:46 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -40,36 +40,6 @@ app.use("/api/gemini",require("./routes/gemini"));
 
 app.use("/api/like",require("./routes/like"));
 
-//fetch authentication using jwt
-app.get("/api/auth/me", async (req,res)=>{
-      const token=req.cookies.token;
-      if (!token) return res.status(404).json({
-          message: "Not Logged in user",
-          user:null
-      })
-      // console.log(res.statusCode);
-  
-      try{
-          const tokenData = jwt.verify(token, process.env.JWT_SECRET);
-          // console.log("Token Data", tokenData);
-  
-          const user=await prisma.user.findUnique({
-              where:{id:tokenData.userId},
-              select:{id:true,username:true},
-          })
-
-          // console.log(user);
-          return res.status(200).json({
-            message: "Logged in user",
-            user: user
-          })
-    
-      }catch(err){
-          res.status(500).json({
-              msg: "error occured"
-          })
-      }
-})
 
 //logout
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const jwt=require("jsonwebtoken");
+
+const {PrismaClient}=require("../generated/prisma");
+const prisma=new PrismaClient();
+
 const {postSignup}=require("../controllers/authController");
 const {postLogin}=require("../controllers/authController");
 
 router.post("/signup",postSignup);
 router.post("/login", postLogin);
-// router.post("/logout", postSignup);
+
+
+//fetch authentication using jwt
+router.get("/me", async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(404).json({
+        message: "Not Logged in user",
+        user: null
+    })
+    // console.log(res.statusCode);
+
+    try {
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET);
+        // console.log("Token Data", tokenData);
+
+        const user = await prisma.user.findUnique({
+            where: { id: tokenData.userId },
+            select: { id: true, username: true },
+        })
+
+        // console.log(user);
+        return res.status(200).json({
+            message: "Logged in user",
+            user: user
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            msg: "error occured"
+        })
+    }
+})
+
 
 module.exports=router;
