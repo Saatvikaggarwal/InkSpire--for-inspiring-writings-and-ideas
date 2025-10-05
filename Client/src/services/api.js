be90f82d322284b3186df2ca574c89078ea027c2
Saatvikaggarwal <124760998+Saatvikaggarwal@users.noreply.github.com>
2025-10-05 10:57:00 +0530

    Update api.js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4444/api",
+        baseUrl: "https://inkspire-for-inspiring-writings-and.onrender.com",
         prepareHeaders: (headers, { getState }) => {
             // cookies sent automatically
             return headers;
